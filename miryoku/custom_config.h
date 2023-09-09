ede828b69c9a53b8ef0f10f5934cef002ad11682
TEEHEETUMMYTUMMS <112745959+TEEHEETUMMYTUMMS@users.noreply.github.com>
2023-09-08 21:47:11 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -17,8 +17,8 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp ESC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSPC  \
-&kp LCTRL K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &mo U_TAP \
-&kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LSHFT \
+&kp LCTRL K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LCTRL \
+&kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &mo U_TAP \
                     K32  K33  K34       K35  K36  K37
 
 #endif
