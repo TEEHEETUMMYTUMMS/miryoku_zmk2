c0cbd4109fc50a7401e337dada72dad8b706fa66
TEEHEETUMMYTUMMS <112745959+TEEHEETUMMYTUMMS@users.noreply.github.com>
2023-09-18 16:12:50 -0400

    lower y

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -56,7 +56,7 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 #define MIRYOKU_LAYER_MOUSE \
 U_WH_U,            U_WH_L,            U_MS_U,            U_WH_R,            U_NU,              &to U_BUTTON,      &to U_BASE,        &to U_EXTRA,       &to U_TAP,         U_BOOT,            \
 U_WH_D,            U_MS_L,            U_MS_D,            U_MS_R,            U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LGUI,          &kp LALT,          \
-&kp LC(Z),         U_CUT,             U_CPY,             U_PST,             &kp LC(y),         U_NA,              &to U_MOUSE,       &to U_NUM,         &kp RALT,          U_NA,              \
+&kp LC(Z),         U_CUT,             U_CPY,             U_PST,             &kp LC(Y),         U_NA,              &to U_MOUSE,       &to U_NUM,         &kp RALT,          U_NA,              \
 U_NP,              U_NP,              U_BTN3,            U_BTN2,            U_BTN1,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 // U_BOOT,            &to U_TAP,         &to U_EXTRA,       &to U_BASE,        &to U_BUTTON,      U_NU,              U_WH_L,            U_MS_U,            U_WH_R,            U_WH_U,            \
