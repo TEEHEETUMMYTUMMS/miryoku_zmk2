8af62d0446b31e5c31d2ba152e8a14feab81a8e5
TEEHEETUMMYTUMMS <112745959+TEEHEETUMMYTUMMS@users.noreply.github.com>
2023-09-20 14:58:52 -0400

    Update miryoku_kludge_mousekeyspr.h

--- miryoku/miryoku_kludge_mousekeyspr.h
+++ miryoku/miryoku_kludge_mousekeyspr.h
@@ -5,9 +5,9 @@
 
 #include <dt-bindings/zmk/mouse.h>
 
-#define U_MOUSE_MOVE_MAX 1500
+#define U_MOUSE_MOVE_MAX 2000
 #define U_MOUSE_MOVE_EXPONENT 2
-#define U_MOUSE_MOVE_TIME 1500
+#define U_MOUSE_MOVE_TIME 500
 #define U_MOUSE_MOVE_DELAY 0
 #define U_MOUSE_SCROLL_MAX 75
 #define U_MOUSE_SCROLL_EXPONENT 1
